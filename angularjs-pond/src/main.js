90b118233ed9f72965492cd7015736897cce8359
aminaouini <aminaouini565@gmail.com>
2019-12-16 10:11:27 +0100

    not done

--- angularjs-pond/src/main.js
+++ angularjs-pond/src/main.js
@@ -1,22 +1,22 @@
 //DO NOT MODIFY THIS FILE
 
-var fishData = [
-  {
+var fishData = {
+  1: {
     name: 'Goldfish',
     image: 'http://tinyurl.com/n4vgcl5',
     description: 'Everyone\'s first pet'
   },
-  {
+  2: {
     name: 'Pufferfish',
     image: 'http://tinyurl.com/kxd7cuu',
     description: 'So puffy!'
   },
-  {
+  3:{
     name: 'Tuna',
     image: 'http://tinyurl.com/zgs7z2s',
     description: 'Why are these things so huge? They\'re terrifying.'
   }
-];
+};
 
 angular.module('fish-pond', [])
 .component('app', {
